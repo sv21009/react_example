d0e7a0a4a7c14ecdc8d68c59ee460084c4ac0ce5
Diego Quinteros <ql17001@ues.edu.sv>
2023-09-26 15:18:53 -0600

    🚨 Update page: Add content

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <main>
+      <h1 className='text-2xl font-bold'>Bienvenidos a la aplicacion react_example</h1>
+      <p>El objetivo de la aplicacion es mostrar el uso de React, componentes, App Router y hooks.</p>
     </main>
   )
 }
