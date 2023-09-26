fb708a7417ed3d612ff1503e0caa7069912f268b
Diego Quinteros <ql17001@ues.edu.sv>
2023-09-26 15:51:26 -0600

    💄 Update Footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='w-full px-6 py-12 bg-blue-500 flex flex-row justify-center text-white'>
+      <a className='hover:underline' href='mailto:ql17001@ues.edu.sv'>Diego Quinteros - 2023</a>
+    </div>
   )
 }
 
