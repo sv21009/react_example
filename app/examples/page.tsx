6e5f12acea16823a7423c8d0079f0b9ca9adf14b
Diego Quinteros <ql17001@ues.edu.sv>
2023-09-26 20:41:14 -0600

    ✨ Add examples page

---
+++ app/examples/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ExamplesHome = () => {
+  return (
+    <div>
+      <h1 className='text-2xl font-bold'>Ejemplos</h1>
+      <p>Esta seccion del sitio web esta destinada a alojar ejemplos en react.</p>
+    </div>
+  )
+}
+
+export default ExamplesHome
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,8 +5,9 @@ const Header = () => {
   return (
     <div className='w-full flex flex-row justify-between p-6 bg-blue-500 text-white'>
       <Link href={'/'} className='text-xl font-bold'>React example</Link>
-      <div>
+      <div className='flex flex-row gap-4'>
         <Link href={'/'}>Inicio</Link>
+        <Link href={'/examples'}>Ejemplos</Link>
       </div>
     </div>
   )
